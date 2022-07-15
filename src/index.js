921f351187b3054cb0a154c6c840977fc71e2e15
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-07-15 13:26:44 +0300

    1

--- src/index.js
+++
@@ -1,2 +0,0 @@
-import { pageSwitcher } from "./js/heder.js";
-pageSwitcher()
--- src/js/heder.js
+++
@@ -1,8 +0,0 @@
-
-export const pageSwitcher = dv => {
-    const home = document.querySelector(".home-js")
-    home.addEventListener('click', homes)
-    function homes () {
-        console.log(home)
-    }
-}
