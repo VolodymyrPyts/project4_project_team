636dddf05a508588949ae31bb341679e7fd819b6
kolesnva <kolesnychenko_v@icloud.com>
2022-07-16 17:07:35 +0300

    done search from input
    added async function which searc firm by keyword from input.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { FetchApi } from "./fetchMain";
 
+// references declaration:
 const refs = {
   form: document.querySelector('.form'),
   searchInput: document.querySelector('input[name=searchQuery]'),
@@ -9,10 +10,20 @@ refs.form.addEventListener('submit', onSearchSubmit);
 
 const fetchFilms = new FetchApi();
 
+// function that searching films from the input
 async function onSearchSubmit(event) {
   event.preventDefault();
 
-  fetchFilms.fetchPopularFilmsByPage();
-
   const request = event.target.elements.searchQuery.value;
+  fetchFilms.currentSearchQuery = request;
+
+  try {
+    const { results } = fetchFilms.fetchSearchFilms();
+
+    console.log(results);
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  event.target.reset();
 }
