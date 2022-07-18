74f0fffad70e1e95731c4240292d8f8f177dfdca
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-07-18 20:48:21 +0300

    Правки

--- src/index.js
+++ src/index.js
@@ -16,26 +16,27 @@ const refs = {
   searchInput: document.querySelector('input[name=searchQuery]'),
 };
 
-// refs.form.addEventListener('submit', onSearchSubmit);
+refs.form.addEventListener('submit', onSearchSubmit);
 
 const fetchFilms = new FetchApi();
 
 // function that searching films from the input
 async function onSearchSubmit(event) {
   event.preventDefault();
+  console.log(event.target.elements)
+  // const request = event.target.elements.searchQuery.value;
+  // fetchFilms.currentSearchQuery = request;
+  
 
-  const request = event.target.elements.searchQuery.value;
-  fetchFilms.currentSearchQuery = request;
+  // try {
+  //   const { results } = fetchFilms.fetchSearchFilms();
 
-  try {
-    const { results } = fetchFilms.fetchSearchFilms();
+  //   console.log(results);
+  // } catch (error) {
+  //   console.log(error.message);
+  // }
 
-    console.log(results);
-  } catch (error) {
-    console.log(error.message);
-  }
-
-  event.target.reset();
+  // event.target.reset();
 }
 
 
@@ -46,4 +47,3 @@ modalFormLogin()
 // import './fetchMain';
 
 
-fonNightDay();
