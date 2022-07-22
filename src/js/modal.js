832cf690aae47b1efc5bf9d623f96c0996b130bb
Roman Radakhevych <akrir@ukr.net>
2022-07-22 22:57:38 +0300

    add check\remove from storage fn

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,4 +1,3 @@
-
 // import { genres } from '../genres.json';
 import { getfilmsGenresUl } from '../filmsListMarkup';
 
@@ -10,74 +9,89 @@ export function modal() {
     modalWrapper: document.querySelector('.modal-wrapper'),
   };
 
-  const watchedArray = [];
-  const queuedArray = [];
+  let watchedArray = localStorage.getItem('Watched')
+    ? JSON.parse(localStorage.getItem('Watched'))
+    : [];
+  let queuedArray = localStorage.getItem('Queued')
+    ? JSON.parse(localStorage.getItem('Queued'))
+    : [];
 
-// клік по карткам, відкриває модалку
-refs.openCards.forEach(item => {
-  item.addEventListener('click', onOpenModal);
-});
+  // клік по карткам, відкриває модалку
+  refs.openCards.forEach(item => {
+    item.addEventListener('click', onOpenModal);
+  });
 
-// клік по кнопці закрити, закриває модалку
-refs.closeBtn.addEventListener('click', onCloseModal);
+  // клік по кнопці закрити, закриває модалку
+  refs.closeBtn.addEventListener('click', onCloseModal);
 
-// клік по бекдропу, закриває модалку
-refs.backdrop.addEventListener('click', onBackdropClick);
+  // клік по бекдропу, закриває модалку
+  refs.backdrop.addEventListener('click', onBackdropClick);
 
-function onBackdropClick(event) {
-  if (event.currentTarget === event.target) {
-    onCloseModal();
-  }
-}
-
-// Тільки при натисканні Escape закривається модалка
-function onEscKeyPress(event) {
-  const ESC_KEY = event.code === 'Escape';
-
-  if (ESC_KEY) {
-    onCloseModal();
-  }
-}
-
-// Функція відкривання модалки. Якщо модалка відкрита, слухаємо подію
-function onOpenModal(event, id) {
-  document.body.style.overflow = 'hidden';
-  window.addEventListener('keydown', onEscKeyPress);
-  refs.backdrop.classList.remove('is-hidden');
-  const currentFilmId = Number(event.currentTarget.id);
-  const movieContainer = document.querySelector('.container-movie-card');
-
-  let watchedBtn;
-
-  let filmData;
-
-  for (let item of JSON.parse(localStorage.getItem('films-request-result'))) {
-    const ID = currentFilmId;
-    if (item.id === ID) {
-      
-      filmData = item;
-      break
+  function onBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+      onCloseModal();
     }
   }
-  const { original_title, title, genre_ids, overview, popularity, poster_path, vote_average, vote_count } = filmData;
-  const filmsGenresList = getfilmsGenresUl(genre_ids).join(', ');
-  
-  const modalMarkup = 
-    `<img class="modal__poster" src=https://image.tmdb.org/t/p/original${poster_path} alt="rectangle"/>
+
+  // Тільки при натисканні Escape закривається модалка
+  function onEscKeyPress(event) {
+    const ESC_KEY = event.code === 'Escape';
+
+    if (ESC_KEY) {
+      onCloseModal();
+    }
+  }
+
+  // Функція відкривання модалки. Якщо модалка відкрита, слухаємо подію
+  function onOpenModal(event, id) {
+    document.body.style.overflow = 'hidden';
+    window.addEventListener('keydown', onEscKeyPress);
+    refs.backdrop.classList.remove('is-hidden');
+    const currentFilmId = Number(event.currentTarget.id);
+    const movieContainer = document.querySelector('.container-movie-card');
+
+    let watchedBtn;
+
+    let filmData;
+
+    for (let item of JSON.parse(localStorage.getItem('films-request-result'))) {
+      const ID = currentFilmId;
+      if (item.id === ID) {
+        filmData = item;
+        break;
+      }
+    }
+    const {
+      original_title,
+      title,
+      genre_ids,
+      overview,
+      popularity,
+      poster_path,
+      vote_average,
+      vote_count,
+    } = filmData;
+    const filmsGenresList = getfilmsGenresUl(genre_ids).join(', ');
+
+    const modalMarkup = `<img class="modal__poster" src=https://image.tmdb.org/t/p/original${poster_path} alt="rectangle"/>
             <div class="modal__movie-data">
                 <p class="modal__movie-title">${title}</p>
                 <table class="modal__table">
                     <tr>
                         <td class="modal__data-title">Vote / Votes</td>
                         <td class="modal__data-info"> 
-                            <span class="rating">${vote_average.toFixed(1)}</span>
+                            <span class="rating">${vote_average.toFixed(
+                              1
+                            )}</span>
                             <span class="slash">/</span>
                             <span class="votes">${Math.round(vote_count)}</span>
                         </td>
                     </tr>
                     <tr>
                         <td class="modal__data-title">Popularity</td>
-                        <td class="modal__data-info">${Math.round(popularity)}</td>
+                        <td class="modal__data-info">${Math.round(
+                          popularity
+                        )}</td>
                     </tr>
                     <tr>
                         <td class="modal__data-title">Original Title</td>
@@ -94,39 +108,106 @@ function onOpenModal(event, id) {
                     </div>
                     <div class="modal__buttons">
                     <button type="submit" class="modal__add-btn watch-btn" id="addToWatch">
-                        <span class="test">ADD&nbsp;TO&nbsp;</span>
+                        <span class="test">${
+                          isFilmInWatched()
+                            ? 'REMOVE&nbsp;FROM&nbsp;'
+                            : 'ADD&nbsp;TO&nbsp;'
+                        }</span>
                         <span class="test">WATCHED</span>
                     </button>
-                    <button type="submit" class="modal__add-btn queue-btn" id="addToQueue">ADD TO QUEUE</button>
+                    <button type="submit" class="modal__add-btn queue-btn" id="addToQueue">${
+                      isFilmInQueue()
+                        ? 'REMOVE&nbsp;FROM&nbsp;'
+                        : 'ADD&nbsp;TO&nbsp;'
+                    }QUEUE</button>
                 </div>`;
-  refs.modalWrapper.innerHTML = modalMarkup;
+    refs.modalWrapper.innerHTML = modalMarkup;
 
-   //Додавання фільмів з модального вікна у локальне сховище
+    //Додавання фільмів з модального вікна у локальне сховище
 
-  const addToWatchedBtn = document.querySelector('#addToWatch');
-  const addToQueueBtn = document.querySelector('#addToQueue');
+    const addToWatchedBtn = document.querySelector('#addToWatch');
+    const addToQueueBtn = document.querySelector('#addToQueue');
 
-  addToWatchedBtn.addEventListener('click', addFilmToWatched);
-  addToQueueBtn.addEventListener('click', addFilmToQueue);
-   
-  function addFilmToWatched() {
-    watchedArray.push(filmData);
-    console.log(watchedArray);
-    localStorage.setItem("Watched", JSON.stringify(watchedArray));
+    addToWatchedBtn.addEventListener('click', watchedFilmHandler);
+    addToQueueBtn.addEventListener('click', queueFilmHandler);
+
+    function watchedFilmHandler() {
+      if (isFilmInWatched()) {
+        removeFilmFromWatched();
+      } else {
+        addFilmToWatched();
+      }
+    }
+
+    function queueFilmHandler() {
+      if (isFilmInQueue()) {
+        removeFilmFromQueue();
+      } else {
+        addFilmToQueue();
+      }
+    }
+
+    function addFilmToWatched() {
+      watchedArray = localStorage.getItem('Watched')
+        ? JSON.parse(localStorage.getItem('Watched'))
+        : [];
+      watchedArray.push(filmData);
+      localStorage.setItem('Watched', JSON.stringify(watchedArray));
+      addToWatchedBtn.textContent = 'REMOVE FROM WATCHED';
+    }
+
+    function addFilmToQueue() {
+      queuedArray = localStorage.getItem('Queued')
+        ? JSON.parse(localStorage.getItem('Queued'))
+        : [];
+      queuedArray.push(filmData);
+      localStorage.setItem('Queued', JSON.stringify(queuedArray));
+      addToQueueBtn.textContent = 'REMOVE FROM QUEUE';
+    }
+
+    function removeFilmFromWatched() {
+      watchedArray = JSON.parse(localStorage.getItem('Watched'));
+      localStorage.setItem(
+        'Watched',
+        JSON.stringify(watchedArray.filter(item => item.id !== currentFilmId))
+      );
+      addToWatchedBtn.textContent = 'ADD TO WATCHED';
+    }
+
+    function removeFilmFromQueue() {
+      queuedArray = JSON.parse(localStorage.getItem('Queued'));
+      localStorage.setItem(
+        'Queued',
+        JSON.stringify(queuedArray.filter(item => item.id !== currentFilmId))
+      );
+      addToQueueBtn.textContent = 'ADD TO QUEUE';
+    }
+
+    function isFilmInWatched() {
+      if (localStorage.getItem('Watched')) {
+        for (let item of JSON.parse(localStorage.getItem('Watched'))) {
+          if (item.id === currentFilmId) {
+            return true;
+          }
+        }
+      }
+    }
+
+    function isFilmInQueue() {
+      if (localStorage.getItem('Queued')) {
+        for (let item of JSON.parse(localStorage.getItem('Queued'))) {
+          if (item.id === currentFilmId) {
+            return true;
+          }
+        }
+      }
+    }
   }
 
-  function addFilmToQueue() {
-    queuedArray.push(filmData);
-    console.log(queuedArray);
-    localStorage.setItem("Queued", JSON.stringify(queuedArray));
+  // Коли модалка закривається, знімаємо слухача подій
+  function onCloseModal() {
+    document.body.style.overflow = '';
+    window.removeEventListener('keydown', onEscKeyPress);
+    refs.backdrop.classList.add('is-hidden');
   }
-}
-
-
-// Коли модалка закривається, знімаємо слухача подій
-function onCloseModal() {
-  document.body.style.overflow = '';
-  window.removeEventListener('keydown', onEscKeyPress);
-  refs.backdrop.classList.add('is-hidden');
-}
 }
